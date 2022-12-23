e383835451aa2530afdd73e1c4fe4d3c7b6c8611
ahmadullahtonmoy <ahmadullahtonmoy@gmail.com>
2022-12-24 00:19:44 +0600

    A little intro to data types

---
+++ bin/data_types.dart
@@ -0,0 +1,9 @@
+// in this field i'll code about dart Data Types.
+/* 
+first of all there are five data types in Dart.
+  1 Numbers (int , double)
+  2 String (String)
+  3 Booleans (bool)
+  4 Lists (List)
+  5 Maps (Map)
+*/
