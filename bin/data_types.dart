0af5537c74b9b6062cffe629feafb28527077a1b
ahmadullahtonmoy <ahmadullahtonmoy@gmail.com>
2022-12-24 00:44:48 +0600

    number , and string type of data added

--- bin/data_types.dart
+++ bin/data_types.dart
@@ -7,3 +7,23 @@ first of all there are five data types in Dart.
   4 Lists (List)
   5 Maps (Map)
 */
+
+void main() {
+  // numbers e.x , int (integer) for non fractional number
+  int _student_roll = 12;
+  int _month_date = 10;
+  int _year = 2022;
+  int _month = 12;
+
+  // double for fractional numbers
+  double _apple_price = 12.5;
+  double _body_weight = 84.43;
+  double _distance = 0.80;
+
+  // strings e.x , use "String" special keyword to declare a string
+  String _user_name = "Hablu";
+  String _place_name = "Green City";
+  String _first_name = "Tom";
+  String _last_name = "Cat";
+  
+}
