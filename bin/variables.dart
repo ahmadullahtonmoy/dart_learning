59bd63a9b5dce4b11284933107a9814290b025bd
ahmadullahtonmoy <ahmadullahtonmoy@gmail.com>
2022-12-24 00:05:49 +0600

    some variables added and see output

--- bin/variables.dart
+++ bin/variables.dart
@@ -1,3 +1,15 @@
 void main() {
-  print("Hello Dart");
+  // declare variables in dart
+  var _banana_price = 12;
+  var _lichi_price = 13;
+  var _student_name = "Sabiha";
+  var _is_hot = false;
+  var _is_winter = true;
+
+  // now time to see output of variables.
+  print("Banana Price is: $_banana_price");
+  print("Lichi Price is: $_lichi_price");
+  print("A Student Name: $_student_name");
+  print("Today Is Warming: $_is_hot");
+  print("Today is Winter: $_is_winter");
 }
